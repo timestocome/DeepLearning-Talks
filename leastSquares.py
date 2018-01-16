515bb8ed22804462e4db4f89b0dc461e8c576e3f
Linda MacPhee-Cobb <timestocome@gmail.com>
2018-01-16 10:46:58 -0600

    added comments to least squares example

--- leastSquares.py
+++ leastSquares.py
@@ -2,6 +2,8 @@
 # http://github.com/timestocome
 # Linear Least Squares
 
+# Example taken from
+# http://mlwiki.org/index.php/OLS_Regression
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,15 +28,23 @@ y = np.asarray([1, 2, 2])
 
 # add row of ones to x, this is our bias
 x_ones = np.ones(len(features))
+
+# x inputs
 X = np.vstack((x_ones, features))
+
+# x vector transposed for dot product
 XT = np.transpose(X)
 
+# initial weight settings
 w = np.ones(2)
+
+# slows down weight adjustments to prevent overshooting mins
 learning_rate = 0.01
 
 # print(X)
 # print(XT)
 
+# X * X
 XTX = np.dot(X, XT)
 # print(XTX)
 
@@ -48,16 +58,23 @@ XTX = np.dot(X, XT)
 # dJ/dw = 2 * XTXw - 2 * XTy
 # w = w - learning_rate * 2 * (XTXW - XTy)
 
+
+# set to any high value or loop will stop on first pass
 error = 999
 last_error = 9999
 
+
+# while error is decreasing
 while (last_error - error) > 0.01:
 
+    # use gradient to decide how much and which way to change weights
     dw = learning_rate * 2 *  (np.dot(XTX, w) - np.dot(X,y))
     w = w - dw
 
+    # test predictions
     y_hat = np.dot(w, X)
 
+    # update error 
     last_error = error
     error = np.sum((y - y_hat)**2)
 
